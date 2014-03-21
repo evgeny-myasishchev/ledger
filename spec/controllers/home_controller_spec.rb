dbdf5ffc4d31cbb5ff845d785ab7cd4cacf70ead
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-03-21 10:20:24 +0200

    Pointing root to home/index.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe HomeController do
+  describe "routes", :type => :routing do
+    it "routes root to home/index" do
+      {get: '/'}.should route_to controller: 'home', action: 'index'
+    end
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
@@ -8,5 +13,4 @@ describe HomeController do
       response.should be_success
     end
   end
-
 end
