f8940f2fb0b793498f9532f47726b14acfa7abde
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-05-15 23:56:33 +0300

    Updating schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319063744) do
+ActiveRecord::Schema.define(version: 20140512195256) do
+
+  create_table "categories", force: true do |t|
+    t.integer "user_id", null: false
+    t.string  "name",    null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
