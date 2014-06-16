2ad47931e51b02c78c4192e0778e4c0ae2f19235
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-16 23:32:22 +0300

    Implementing ledger projection.

--- app/models/projections/ledger.rb
+++ app/models/projections/ledger.rb
@@ -1,2 +1,42 @@
 class Projections::Ledger < ActiveRecord::Base
+  include CommonDomain::Projections::ActiveRecord
+  include Domain::Events
+  include Projections
+  
+  serialize :shared_with_user_ids, Set
+  
+  projection do
+    on LedgerCreated do |event|
+      Ledger.create!(aggregate_id: event.aggregate_id, owner_user_id: event.user_id, name: event.name) unless
+        Ledger.exists?(aggregate_id: event.aggregate_id)
+    end
+    
+    on LedgerRenamed do |event|
+      Ledger.where(aggregate_id: event.aggregate_id).update_all name: event.name
+    end
+    
+    on LedgerShared do |event|
+      ledger = Ledger.find_by_aggregate_id event.aggregate_id
+      ledger.shared_with_user_ids.add event.user_id
+      ledger.save!
+    end
+  end
+  
+  
+  #   projection do
+  #     on Events::EmployeeCreated do |event|
+  #       Employee.create! employee_id: event.aggregate_id, name: event.name
+  #     end
+  #   
+  #     on Events::EmployeeRenamed do |event|
+  #       rec = Employee.find_by(employee_id: event.aggregate_id)
+  #       rec.name = event.name
+  #       rec.save!
+  #     end
+  #   
+  #     on Events::EmployeeRemoved do |event|
+  #       Employee.where(employee_id: event.aggregate_id).delete_all
+  #     end
+  #   end
+  
 end
--- spec/models/projections/ledger_spec.rb
+++ spec/models/projections/ledger_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Projections::Ledger, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.create_projection }
+  
+  before(:each) do
+    subject.handle_message Domain::Events::LedgerCreated.new 'ledger-1', 100, 'Ledger 1'
+  end
+  
+  let(:ledger_1) { ledger = described_class.find_by_aggregate_id 'ledger-1' }
+  
+  describe "LedgerCreated" do
+    it "should create corresponding ledger record" do
+      ledger_1 = described_class.find_by aggregate_id: 'ledger-1'
+      expect(ledger_1.name).to eql 'Ledger 1'
+      expect(ledger_1.owner_user_id).to eql 100
+    end
+    
+    it "should be idempotent" do
+      expect { subject.handle_message Domain::Events::LedgerCreated.new 'ledger-1', 100, 'Ledger 1' }.not_to change { described_class.count }
+    end
+  end
+  
+  describe "LedgerRenamed" do
+    it "should rename the ledger" do
+      subject.handle_message Domain::Events::LedgerRenamed.new 'ledger-1', 'Ledger 110'
+    end
+  end
+  
+  describe "LedgerShared" do
+    before(:each) do
+      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 120
+      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 130
+    end
+    
+    it "should record corresponding user_id" do
+      expect(ledger_1.shared_with_user_ids).to eql Set.new([120, 130])
+    end
+    
+    it "should be idempotent" do
+      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 120
+      expect(ledger_1.shared_with_user_ids).to eql Set.new([120, 130])
+    end
+  end
 end
