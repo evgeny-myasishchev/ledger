0bd4695fd5a68af76d16db199c2707fc2792d81b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-16 23:39:48 +0300

    Some optimization.

--- spec/models/projections/ledger_spec.rb
+++ spec/models/projections/ledger_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Projections::Ledger, :type => :model do
   subject { described_class.create_projection }
+  let(:e) { Domain::Events }
   
   before(:each) do
-    subject.handle_message Domain::Events::LedgerCreated.new 'ledger-1', 100, 'Ledger 1'
+    subject.handle_message e::LedgerCreated.new 'ledger-1', 100, 'Ledger 1'
   end
   
   let(:ledger_1) { ledger = described_class.find_by_aggregate_id 'ledger-1' }
@@ -17,20 +18,20 @@ RSpec.describe Projections::Ledger, :type => :model do
     end
     
     it "should be idempotent" do
-      expect { subject.handle_message Domain::Events::LedgerCreated.new 'ledger-1', 100, 'Ledger 1' }.not_to change { described_class.count }
+      expect { subject.handle_message e::LedgerCreated.new 'ledger-1', 100, 'Ledger 1' }.not_to change { described_class.count }
     end
   end
   
   describe "LedgerRenamed" do
     it "should rename the ledger" do
-      subject.handle_message Domain::Events::LedgerRenamed.new 'ledger-1', 'Ledger 110'
+      subject.handle_message e::LedgerRenamed.new 'ledger-1', 'Ledger 110'
     end
   end
   
   describe "LedgerShared" do
     before(:each) do
-      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 120
-      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 130
+      subject.handle_message e::LedgerShared.new 'ledger-1', 120
+      subject.handle_message e::LedgerShared.new 'ledger-1', 130
     end
     
     it "should record corresponding user_id" do
@@ -38,7 +39,7 @@ RSpec.describe Projections::Ledger, :type => :model do
     end
     
     it "should be idempotent" do
-      subject.handle_message Domain::Events::LedgerShared.new 'ledger-1', 120
+      subject.handle_message e::LedgerShared.new 'ledger-1', 120
       expect(ledger_1.shared_with_user_ids).to eql Set.new([120, 130])
     end
   end
