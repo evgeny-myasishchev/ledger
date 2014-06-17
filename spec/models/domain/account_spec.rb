feef4fb7b5a6cd01d536cee220988429a2e169fc
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-18 01:01:16 +0300

    Adding transaction date. Fixing tests. Attributes must match exactly.

--- spec/models/domain/account_spec.rb
+++ spec/models/domain/account_spec.rb
@@ -11,7 +11,11 @@ describe Domain::Account do
       expect(CommonDomain::Infrastructure::AggregateId).to receive(:new_id).and_return('account-100')
       currency = Currency['UAH']
       subject.create 'ledger-100', 'Account 100', currency
-      expect(subject).to have_one_uncommitted_event I::AccountCreated, ledger_id: 'ledger-100', name: 'Account 100', currency_code: currency.code
+      expect(subject).to have_one_uncommitted_event I::AccountCreated, 
+        aggregate_id: subject.aggregate_id,
+        ledger_id: 'ledger-100', 
+        name: 'Account 100', 
+        currency_code: currency.code
     end
     
     it "should assign the aggregate_id on created event" do
@@ -23,7 +27,7 @@ describe Domain::Account do
   describe "rename" do
     it "should raise AccountRenamed event" do
       subject.make_created.rename 'Account 332 renamed'
-      expect(subject).to have_one_uncommitted_event I::AccountRenamed, name: 'Account 332 renamed'
+      expect(subject).to have_one_uncommitted_event I::AccountRenamed, aggregate_id: subject.aggregate_id, name: 'Account 332 renamed'
     end
   end
   
@@ -34,27 +38,44 @@ describe Domain::Account do
     
     it "should raise AccountClosed event" do
       subject.close
-      expect(subject).to have_one_uncommitted_event I::AccountClosed, {}
+      expect(subject).to have_one_uncommitted_event I::AccountClosed, aggregate_id: subject.aggregate_id
     end
     
     it "should raise nothing if already closed" do
       subject.apply_event I::AccountClosed.new 'account-332'
+      expect(subject).not_to have_uncommitted_events
     end
   end
   
   describe "report_income" do
     it "should raise TransactionReported event" do
       expect(CommonDomain::Infrastructure::AggregateId).to receive(:new_id).and_return('transaction-100')
-      subject.make_created.report_income '10.40', ['t-1', 't-2'], 'Monthly income'
-      expect(subject).to have_one_uncommitted_event I::TransactionReported, transaction_id: 'transaction-100', type_id: Domain::Transaction::IncomeTypeId, ammount: 1040
+      date = DateTime.now
+      subject.make_created.report_income '10.40', date, ['t-1', 't-2'], 'Monthly income'
+      expect(subject).to have_one_uncommitted_event I::TransactionReported, 
+        aggregate_id: subject.aggregate_id, 
+        transaction_id: 'transaction-100',
+        type_id: Domain::Transaction::IncomeTypeId,
+        ammount: 1040,
+        date: date,
+        tag_ids: ['t-1', 't-2'],
+        comment: 'Monthly income'
     end
   end
     
   describe "report_expence" do
     it "should raise TransactionReported event" do
       expect(CommonDomain::Infrastructure::AggregateId).to receive(:new_id).and_return('transaction-100')
-      subject.make_created.report_expence 2023, ['t-1', 't-2'], 'Monthly income'
-      expect(subject).to have_one_uncommitted_event I::TransactionReported, transaction_id: 'transaction-100', type_id: Domain::Transaction::ExpenceTypeId, ammount: 2023
+      date = DateTime.now
+      subject.make_created.report_expence 2023, date, ['t-1', 't-2'], 'Monthly income'
+      expect(subject).to have_one_uncommitted_event I::TransactionReported, 
+        aggregate_id: subject.aggregate_id, 
+        transaction_id: 'transaction-100',
+        type_id: Domain::Transaction::ExpenceTypeId,
+        ammount: 2023,
+        date: date,
+        tag_ids: ['t-1', 't-2'],
+        comment: 'Monthly income'
     end
   end
   
