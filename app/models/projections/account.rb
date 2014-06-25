27d4aace82861d6ced8ff6d8c979fb9fe4f9b687
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-25 23:12:33 +0300

    Extending and refactoring to simplify query.

--- app/models/projections/account.rb
+++ app/models/projections/account.rb
@@ -7,10 +7,30 @@ class Projections::Account < ActiveRecord::Base
     
   end
   
+  def authorize_user(user_id)
+    authorized_user_ids_will_change!
+    authorized_user_ids << ',' unless authorized_user_ids.empty?
+    authorized_user_ids << '{' 
+    authorized_user_ids << user_id.to_s
+    authorized_user_ids << '}'
+  end
+  
   projection do
+    on LedgerShared do |event|
+      Account.where(ledger_id: event.aggregate_id).each { |a|
+        a.authorize_user event.user_id
+        a.save!
+      }
+    end
+    
     on AccountCreated do |event|
+      ledger = Ledger.find_by_aggregate_id event.ledger_id
+      authorized_user_ids = ledger.shared_with_user_ids
+      authorized_user_ids.add ledger.owner_user_id
       Account.create!(aggregate_id: event.aggregate_id, 
         ledger_id: event.ledger_id, 
+        owner_user_id: ledger.owner_user_id,
+        authorized_user_ids: authorized_user_ids.map { |id| "{#{id}}" }.join(','),
         name: event.name, 
         currency_code: event.currency_code, 
         balance: 0,
--- spec/models/projections/account_spec.rb
+++ spec/models/projections/account_spec.rb
@@ -1,19 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Projections::Account, :type => :model do
+  let(:p) { Projections }
+  let(:ledger) {
+    p::Ledger.create!(aggregate_id: 'ledger-1', owner_user_id: 22331, shared_with_user_ids: Set.new([22332, 22333]), name: 'ledger 1')
+  }
+  
+  describe "authorize_user" do
+    it "should add user id to a list of ids" do
+      a = create_account! 'a-2233', ledger.aggregate_id, authorized_user_ids: '{22},{33}'
+      a.authorize_user 44
+      a.authorize_user 45
+      expect(a.authorized_user_ids).to eql "{22},{33},{44},{45}"
+      expect(a.authorized_user_ids_changed?).to be_truthy
+      
+      a = create_account! 'a-1', ledger.aggregate_id, authorized_user_ids: ''
+      a.authorize_user 10
+      expect(a.authorized_user_ids).to eql "{10}"
+    end
+  end
   
   describe "projection" do
     subject { described_class.create_projection }
     let(:e) { Domain::Events }
   
     before(:each) do
-      subject.handle_message e::AccountCreated.new 'account-223', 'ledger-1', 'Account 223', 'UAH'
+      subject.handle_message e::AccountCreated.new 'account-223', ledger.aggregate_id, 'Account 223', 'UAH'
     end
     let(:account_223) { described_class.find_by_aggregate_id 'account-223' }
+    
     describe "on AccountCreated" do
       it "should create corresponding record" do
         expect(account_223).not_to be_nil
-        expect(account_223.ledger_id).to eql 'ledger-1'
+        expect(account_223.ledger_id).to eql ledger.aggregate_id
+        expect(account_223.owner_user_id).to eql ledger.owner_user_id
+        expect(account_223.authorized_user_ids).to eql "{22332},{22333},{22331}"
         expect(account_223.currency_code).to eql 'UAH'
         expect(account_223.name).to eql 'Account 223'
         expect(account_223.balance).to eql 0
@@ -24,7 +45,20 @@ RSpec.describe Projections::Account, :type => :model do
         expect { subject.handle_message e::AccountCreated.new 'account-223', 'ledger-1', 'Account 223', 'UAH' }.not_to change { described_class.count }
       end
     end
-  
+    
+    describe "on LedgerShared" do
+      it "should add user id to a list of users for all accounts" do
+        a1 = create_account! 'a-1', ledger.aggregate_id
+        a2 = create_account! 'a-2', ledger.aggregate_id
+        subject.handle_message e::LedgerShared.new ledger.aggregate_id, 110
+        subject.handle_message e::LedgerShared.new ledger.aggregate_id, 120
+        a1.reload
+        a2.reload
+        expect(a1.authorized_user_ids).to eql "{100},{110},{120}"
+        expect(a2.authorized_user_ids).to eql "{100},{110},{120}"
+      end
+    end
+    
     describe "on AccountRenamed" do
       it "should update the name" do
         subject.handle_message e::AccountRenamed.new 'account-223', 'New Name 223'
@@ -38,5 +72,16 @@ RSpec.describe Projections::Account, :type => :model do
         expect(account_223.is_closed).to be_truthy
       end
     end
+  end
+  
+  def create_account! aggregate_id, ledger_id, owner_user_id = 100, authorized_user_ids: "{100}"
+    p::Account.create! aggregate_id: aggregate_id,
+      ledger_id: ledger_id,
+      owner_user_id: 100,
+      authorized_user_ids: authorized_user_ids,
+      currency_code: 'UAH',
+      name: 'A 1',
+      balance: 0,
+      is_closed: false
   end
 end
