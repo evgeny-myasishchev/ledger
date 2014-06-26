ceeecbccb48f51777ddd12f58f697696d339a285
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-26 23:55:43 +0300

    Reworking the template. Implementing selection.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,9 @@
 ledgerApp.controller('HomeController', function ($scope, accounts) {
 	$scope.accounts = accounts;
+	$scope.selectAccount = function(account) {
+		$scope.activeAccount = account;
+	};
+	$scope.isActive = function(account) {
+		return $scope.activeAccount == account;
+	};
 });
--- spec/javascripts/home_spec.js
+++ spec/javascripts/home_spec.js
@@ -1,17 +1,38 @@
 describe("HomeController", function() {
+	var account1, account2, account3;
+	var controller, scope;
 	beforeEach(function() {
 		module('ledgerApp');
 		ledgerApp.constant('accounts',  [
-			{'name': 'Cache UAH', 'balance': '100 UAH'},
-			{'name': 'PC Credit J', 'balance': '200 UAH'},
-			{'name': 'VAB Visa', 'balance': '4432 UAH'}
+			account1 = {id: 1, 'name': 'Cache UAH', 'balance': '100 UAH'},
+			account2 = {id: 2, 'name': 'PC Credit J', 'balance': '200 UAH'},
+			account3 = {id: 3, 'name': 'VAB Visa', 'balance': '4432 UAH'}
 		]);
+		inject(function($controller) {
+			scope = {};
+			controller = $controller('HomeController', {$scope: scope});
+		});
 	});
 	
-	
-	it("should have default accounts", inject(function($controller) {
-		var scope = {};
-		var controller = $controller('HomeController', {$scope: scope});
+	it("should have default accounts", function() {
 		expect(scope.accounts.length).toEqual(3);
-	}));
+	});
+	
+	describe("selectAccount", function() {
+		beforeEach(function() {
+			scope.selectAccount(account1);
+		});
+		
+		it("should assign activeAccount into the scope", function() {
+			expect(scope.activeAccount).toEqual(account1);
+		});
+	});
+	
+	describe('isActive', function() {
+		it('should determine if the account is active', function() {
+			expect(scope.isActive(account1)).toBeFalsy();
+			scope.activeAccount = account1;
+			expect(scope.isActive(account1)).toBeTruthy();
+		});
+	});
 });
