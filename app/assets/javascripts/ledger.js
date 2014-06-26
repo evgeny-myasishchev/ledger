c4770b44782aabdf2b7b43edc9b376bb636b9c5f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-06-26 23:55:24 +0300

    Adding error handling.

--- app/assets/javascripts/ledger.js
+++ app/assets/javascripts/ledger.js
@@ -1,1 +1,7 @@
-var ledgerApp = angular.module('ledgerApp', []);
+angular.module('ErrorLogger', []).factory('$exceptionHandler', function () {
+	return function errorCatcherHandler(exception, cause) {
+		console.error(exception);
+	};
+});
+
+var ledgerApp = angular.module('ledgerApp', ['ErrorLogger']);
