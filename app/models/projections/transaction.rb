49c160c765ad0c599597c84e136ee792ecf45cb2
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-07-01 01:00:59 +0300

    Reworking logic to add tag_ids, implementing read method to get transactions.

--- app/models/projections/transaction.rb
+++ app/models/projections/transaction.rb
@@ -3,18 +3,35 @@ class Projections::Transaction < ActiveRecord::Base
   include Domain::Events
   include Projections
   
-  serialize :tag_ids, Set
+  def add_tag(the_id)
+    wrapped_tag_id = "{#{the_id}}"
+    if !self.tag_ids.nil? && self.tag_ids.include?(wrapped_tag_id)
+      return
+    end
+    result = self.tag_ids || ""
+    result << ',' unless result.blank?
+    result << wrapped_tag_id
+    self.tag_ids = result
+  end
+  
+  def self.get_account_transactions(user, account_id)
+    Account.ensure_authorized! account_id, user
+    Transaction.
+      where('account_id = :account_id', account_id: account_id).
+      select(:id, :transaction_id, :type_id, :ammount, :balance, :tag_ids, :comment, :date)
+  end
   
   projection do
     on TransactionReported do |event|
-      Transaction.create! account_id: event.aggregate_id,
+      t = Transaction.new account_id: event.aggregate_id,
         transaction_id: event.transaction_id,
         type_id: event.type_id,
         ammount: event.ammount,
         balance: event.balance,
-        tag_ids: Set.new(event.tag_ids),
         comment: event.comment,
         date: event.date
+      event.tag_ids.each { |tag_id| t.add_tag tag_id }
+      t.save!
     end
   end
 end
--- spec/models/projections/transaction_spec.rb
+++ spec/models/projections/transaction_spec.rb
@@ -3,9 +3,71 @@ require 'rails_helper'
 RSpec.describe Projections::Transaction, :type => :model do
   subject { described_class.create_projection }
   let(:e) { Domain::Events }
+  let(:p) { Projections }
   let(:income_id) { Domain::Transaction::IncomeTypeId }
   let(:expence_id) { Domain::Transaction::ExpenceTypeId }
+  include AccountHelpers::P
   
+  describe "self.get_account_transactions" do
+    let(:user) { 
+      u = User.new
+      u.id = 2233
+      u
+    }
+    let(:date) { DateTime.now }
+    before(:each) do
+      create_account_projection! 'account-1', authorized_user_ids: '{100},{2233},{12233}'
+      subject.handle_message e::TransactionReported.new 'account-1', 't-1', income_id, 10523, 22003, date - 100, ['t-1', 't-2'], 'Comment 101'
+      subject.handle_message e::TransactionReported.new 'account-1', 't-2', expence_id, 2000, 20003, date - 110, ['t-4'], 'Comment 102'
+      subject.handle_message e::TransactionReported.new 'account-1', 't-3', expence_id, 2000, 18003, date - 120, ['t-4'], 'Comment 103'
+      allow(p::Account).to receive(:ensure_authorized!)
+    end
+    
+    it "should check if the user is authorized" do
+      described_class.get_account_transactions user, 'account-1'
+      expect(p::Account).to have_received(:ensure_authorized!).with('account-1', user)
+    end
+    
+    it "should get all transactions of the user" do
+      transactions = described_class.get_account_transactions user, 'account-1'
+      expect(transactions.length).to eql 3
+      t1 = transactions.detect { |t| t.transaction_id == 't-1' }
+      expect(t1.attributes).to eql('id' => t1.id,
+        'transaction_id' => 't-1',
+        'type_id' => income_id,
+        'ammount' => 10523,
+        'balance' => 22003,
+        'tag_ids' => '{t-1},{t-2}',
+        'comment' => 'Comment 101',
+        'date' => (date - 100).to_time)
+      expect(transactions.detect { |t| t.transaction_id == 't-2' }).not_to be_nil
+      expect(transactions.detect { |t| t.transaction_id == 't-3' }).not_to be_nil
+    end
+  end
+  
+  describe "add_tag" do
+    subject { p::Transaction.new }
+    before(:each) do
+      subject.add_tag 100
+      subject.add_tag 110
+      subject.add_tag 120
+    end
+    
+    it "should add a tag wrapped in curly braces" do
+      expect(subject.tag_ids).to eql '{100},{110},{120}'
+    end
+    
+    it "should mark the tag_ids as changed" do
+      expect(subject.tag_ids_changed?).to be_truthy
+    end
+    
+    it "should not add the tag_id if already present" do
+      subject.changed_attributes.clear
+      subject.add_tag 100
+      expect(subject.tag_ids).to eql('{100},{110},{120}')
+      expect(subject.tag_ids_changed?).to be_falsey
+    end
+  end
   
   describe "on TransactionReported" do
     it "should record the transaction" do
@@ -19,16 +81,16 @@ RSpec.describe Projections::Transaction, :type => :model do
       expect(t1.type_id).to eql(income_id)
       expect(t1.ammount).to eql(10523)
       expect(t1.balance).to eql(22003)
-      expect(t1.tag_ids).to eql Set.new(['t-1', 't-2'])
+      expect(t1.tag_ids).to eql '{t-1},{t-2}'
       expect(t1.comment).to eql 'Comment 100'
       expect(t1.date.to_datetime).to eql date1.utc
-            
+      
       t2 = described_class.find_by_transaction_id 't-2'
       expect(t2.account_id).to eql('account-1')
       expect(t2.type_id).to eql(expence_id)
       expect(t2.ammount).to eql(2000)
       expect(t2.balance).to eql(20003)
-      expect(t2.tag_ids).to eql Set.new(['t-3', 't-4'])
+      expect(t2.tag_ids).to eql '{t-3},{t-4}'
       expect(t2.comment).to eql 'Comment 101'
       expect(t2.date.to_datetime).to eql date2.utc
     end
