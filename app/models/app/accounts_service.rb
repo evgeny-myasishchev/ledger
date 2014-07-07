0e7dc2b3d70ef0ec99e61edc33c1f8e805a5ca21
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-07-07 22:28:36 +0300

    Adding pending stuff to report transactions.

---
+++ app/models/app/accounts_service.rb
@@ -0,0 +1,2 @@
+class App::AccountsService < CommonDomain::CommandHandler
+end
---
+++ app/models/app/commands.rb
@@ -0,0 +1,8 @@
+module App::Commands
+  include CommonDomain::Command::DSL
+  
+  commands_group :AccountCommands do
+    command :ReportIncome, :account_id, :ammount, :date, :tag_ids, :comment
+    command :ReportExpence, :account_id, :ammount, :date, :tag_ids, :comment
+  end
+end
