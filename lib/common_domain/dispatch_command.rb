096f522ca400bc6a3c4d71a590079d2549508d7f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-07-09 22:51:53 +0300

    Adding comment.

--- lib/common_domain/dispatch_command.rb
+++ lib/common_domain/dispatch_command.rb
@@ -1,4 +1,6 @@
 module CommonDomain
+  
+  # Include the module to your controller and feel free to dispatch commands using the dispatch_command method.
   module DispatchCommand
     def dispatch_command command
       domain_context.dispatch_middleware.call(command, dispatch_context)
