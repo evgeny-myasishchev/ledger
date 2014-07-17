79084a912569599b8dc12340dc6fe74643f92d96
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2014-07-17 13:46:23 +0300

    Registering the tags projection.

--- lib/domain_context.rb
+++ lib/domain_context.rb
@@ -36,6 +36,7 @@ class DomainContext < CommonDomain::DomainContext
       projections.register :ledgers, ::Projections::Ledger.create_projection
       projections.register :accounts, ::Projections::Account.create_projection
       projections.register :transactions, ::Projections::Transaction.create_projection
+      projections.register :tags, ::Projections::Tag.create_projection
     end
   end
 end
