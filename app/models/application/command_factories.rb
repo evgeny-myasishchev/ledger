231752a73783f795fd8b00cca8fcceb21e85fd36
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-07-21 22:16:02 +0300

    Implementing transfer command factory.

--- app/models/application/command_factories.rb
+++ app/models/application/command_factories.rb
@@ -19,7 +19,22 @@ module Application::CommandFactories
     extend ActiveSupport::Concern
     module ClassMethods
       def build_from_params params
-        raise "Not implemented"
+        account_id = params[:account_id]
+        receiving_account_id = params[:receiving_account_id]
+        ammount_sent = params[:command][:ammount_sent]
+        ammount_received = params[:command][:ammount_received]
+        date = params[:command][:date]
+        raise ArgumentError.new 'account_id is missing' if account_id.blank?
+        raise ArgumentError.new 'receiving_account_id is missing' if receiving_account_id.blank?
+        raise ArgumentError.new 'ammount_sent is missing' if ammount_sent.blank?
+        raise ArgumentError.new 'ammount_received is missing' if ammount_received.blank?
+        raise ArgumentError.new 'date is missing' if date.blank?
+        new account_id, receiving_account_id: receiving_account_id,
+          ammount_sent: ammount_sent,
+          ammount_received: ammount_received,
+          date: DateTime.iso8601(date),
+          tag_ids: params[:command][:tag_ids],
+          comment: params[:command][:comment]
       end
     end
   end
--- spec/models/command_factories_spec.rb
+++ spec/models/command_factories_spec.rb
@@ -60,6 +60,72 @@ describe Application::Commands do
     end
   end
   
+  describe described_class::TransferCommandFactory do
+    let(:described_class) {
+      Class.new(CommonDomain::Command) do
+        include Application::Commands::TransferCommandFactory
+        attr_reader :receiving_account_id, :ammount_sent, :ammount_received, :date, :tag_ids, :comment
+      end
+    }
+  
+    describe "build_from_params" do
+      let(:params) { Hash.new }
+      let(:date) { 
+        d = (DateTime.now - 100)
+        d = d.iso8601
+        DateTime.iso8601 d
+      }
+      before(:each) do
+        params[:account_id] = 'sending-993'
+        params[:receiving_account_id] = 'receiving-2291'
+        params[:command] = {
+          ammount_sent: '22110',
+          ammount_received: '100110',
+          date: date.iso8601
+        }
+      end
+    
+      subject { described_class.build_from_params params }
+    
+      it "should assign command attributes" do
+        params[:command][:tag_ids] = ['t-1', 't-2']
+        params[:command][:comment] = 'Command comment 201120'
+        expect(subject.aggregate_id).to eql 'sending-993'
+        expect(subject.receiving_account_id).to eql 'receiving-2291'
+        expect(subject.ammount_sent).to eql '22110'
+        expect(subject.ammount_received).to eql '100110'
+        expect(subject.date).to eql date
+        expect(subject.tag_ids).to eql ['t-1', 't-2']
+        expect(subject.comment).to eql 'Command comment 201120'
+      end
+      
+      it "should fail if the account_id is not specified" do
+        params[:account_id] = nil
+        expect{subject}.to raise_error ArgumentError, 'account_id is missing'
+      end
+      
+      it "should fail if the receiving_account_id is not specified" do
+        params[:receiving_account_id] = nil
+        expect{subject}.to raise_error ArgumentError, 'receiving_account_id is missing'
+      end
+    
+      it "should fail if ammount_sent is not specified" do
+        params[:command][:ammount_sent] = nil
+        expect{subject}.to raise_error ArgumentError, 'ammount_sent is missing'
+      end
+    
+      it "should fail if ammount_received is not specified" do
+        params[:command][:ammount_received] = nil
+        expect{subject}.to raise_error ArgumentError, 'ammount_received is missing'
+      end
+    
+      it "should fail if date is not specified" do
+        params[:command][:date] = nil
+        expect{subject}.to raise_error ArgumentError, 'date is missing'
+      end
+    end
+  end
+  
   describe described_class::AccountCommands::ReportIncome do
     it "should include the IncomeExpenceCommandFactory" do
       expect(subject).to be_a(Application::Commands::IncomeExpenceCommandFactory)
@@ -69,6 +135,18 @@ describe Application::Commands do
   describe described_class::AccountCommands::ReportExpence do
     it "should include the IncomeExpenceCommandFactory" do
       expect(subject).to be_a(Application::Commands::IncomeExpenceCommandFactory)
+    end
+  end
+  
+  describe described_class::AccountCommands::ReportRefund do
+    it "should include the IncomeExpenceCommandFactory" do
+      expect(subject).to be_a(Application::Commands::IncomeExpenceCommandFactory)
+    end
+  end
+    
+  describe described_class::AccountCommands::ReportTransfer do
+    it "should include the TransferCommandFactory" do
+      expect(subject).to be_a(Application::Commands::TransferCommandFactory)
     end
   end
 end
