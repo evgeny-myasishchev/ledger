57c9cd94c4d1e5eb12a957921c8bda27ada1f620
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2014-08-10 17:02:48 +0300

    Adding accounts controller relating actions.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,19 @@
 class AccountsController < ApplicationController
+  include Application::Commands::LedgerCommands
+  include Application::Commands::AccountCommands
+  
+  def create
+    dispatch_command CreateNewAccount.new params[:ledger_id], params
+    render nothing: true
+  end
+  
+  def close
+    dispatch_command CloseAccount.new params[:ledger_id], params
+    render nothing: true
+  end
+  
+  def rename
+    dispatch_command RenameAccount.from_hash params
+    render nothing: true
+  end
 end
--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AccountsController, :type => :controller do
-  let(:cmd) { Application::Commands::AccountCommands }
+  let(:i) {
+    Module.new do
+      include Application::Commands::LedgerCommands
+      include Application::Commands::AccountCommands
+    end
+  }
   include AuthenticationHelper
+  authenticate_user
   
   describe "routes", :type => :routing do
     it "routes ledgers nested new route" do
@@ -19,6 +25,41 @@ RSpec.describe AccountsController, :type => :controller do
     
     it "routes rename" do
       expect({put: 'accounts/33223/rename'}).to route_to controller: 'accounts', action: 'rename', aggregate_id: '33223'
+    end
+  end
+  
+  describe "ledger nested actions" do
+    def should_dispatch verb, action, command_class
+      command = double(:command)
+      expect(command_class).to receive(:new) do |aggregate_id, params|
+        expect(aggregate_id).to eql 'ledger-221'
+        expect(params).to be controller.params
+        command
+      end
+      expect(controller).to receive(:dispatch_command).with(command)
+      send verb, action, ledger_id: 'ledger-221', account_id: 'account-9932', key1: 'value-1', key2: 'value-2'
+      expect(response.status).to eql 200
+    end
+    
+    it "should disaptch create command on POST 'create'" do
+      should_dispatch :post, 'create', i::CreateNewAccount
+    end
+    
+    it "should disaptch close command on POST 'close'" do
+      should_dispatch :post, 'close', i::CloseAccount
+    end
+  end
+  
+  describe "PUT 'rename'" do
+    it "should dispatch rename command" do
+      command = double(:command)
+      expect(i::RenameAccount).to receive(:from_hash) do |params|
+        expect(params).to be controller.params
+        command
+      end
+      expect(controller).to receive(:dispatch_command).with(command)
+      put 'rename', aggregate_id: 'account-223', key1: 'value-1', key2: 'value-2'
+      expect(response.status).to eql 200
     end
   end
 end
