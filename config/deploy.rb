f2587b7d6172fed4fb31d41c33df5f3080c3a964
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-01-06 11:15:16 +0200

    Disabling binstubs. They're not really needed but causing capistrano issues.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,10 @@ set :repo_url, 'https://github.com/evgeny-myasishchev/ledger.git'
 # Default value for :linked_files is []
 set :linked_files, %w{.env config/database.yml}
 
+set :bundle_binstubs, nil
+
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
