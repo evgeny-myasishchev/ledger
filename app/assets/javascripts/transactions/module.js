40ca7f6d03b6913f5907fbc2bc641c2d9491c5f1
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-01-07 23:50:55 +0200

    Optimizing templates structure.

--- app/assets/javascripts/transactions/module.js
+++ app/assets/javascripts/transactions/module.js
@@ -2,7 +2,7 @@ var transactionsApp = (function() {
 	var transactionsApp = angular.module('transactionsApp', ['ErrorHandler', 'ngRoute', 'ledgerHelpers', 'accountsApp']);
 	transactionsApp.config(['$routeProvider', function($routeProvider) {
 			$routeProvider.when('/accounts/:accountSequentialNumber/report', {
-				templateUrl: "report.html",
+				templateUrl: "report-transactions.html",
 				controller: 'ReportTransactionsController'
 			});
 		}
