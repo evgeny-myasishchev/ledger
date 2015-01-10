9b811e04169dd8452e93e91b447e8a509dcba3f2
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-01-10 02:02:42 +0200

    Optimizing files structure. Reducing number of files.

--- app/assets/javascripts/transactions/index.js
+++ app/assets/javascripts/transactions/index.js
@@ -1,2 +1,18 @@
-//= require transactions/module
+//= require_self
 //= require_tree .
+
+var transactionsApp = (function() {
+	var transactionsApp = angular.module('transactionsApp', ['ErrorHandler', 'ngRoute', 'UUID', 'ledgerHelpers', 'accountsApp']);
+	transactionsApp.config(['$routeProvider', function($routeProvider) {
+			$routeProvider.when('/report', {
+				templateUrl: "report-transactions.html",
+				controller: 'ReportTransactionsController'
+			});
+			$routeProvider.when('/accounts/:accountSequentialNumber/report', {
+				templateUrl: "report-transactions.html",
+				controller: 'ReportTransactionsController'
+			});
+		}
+	]);
+	return transactionsApp;
+})();
