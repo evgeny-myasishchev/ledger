c62d66e29f989e23a503e4ed25e4c506e906df17
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-03-18 01:30:27 +0200

    Implementing authentication by token.

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,23 @@
+require 'google-id-token-extractor'
+
 class Api::SessionsController < ApplicationController
   
   skip_filter :authenticate_user!
+  protect_from_forgery except: :create
   
   def create
+    if params[:google_id_token]
+      token = GoogleIDToken::Extractor.extract(params[:google_id_token])
+      user = User.find_by email: token['email']
+      sign_in :user, user if user
+    end
     respond_to do |format|
       format.json {
-        render json: {form_authenticity_token: form_authenticity_token}
+        if user_signed_in?
+          render json: {form_authenticity_token: form_authenticity_token}
+        else
+          render nothing: true, status: :unauthorized
+        end
       }
     end
   end
--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -9,8 +9,43 @@ RSpec.describe Api::SessionsController, :type => :controller do
   end
   
   describe "POST create" do
-    it 'should return authenticity token' do
+    let(:token_data) { 
+      {'email' => 'user@domain.com'}
+    }
+    let(:dummy_user) { User.new id: 12432121, email: token_data['email'] }
+    before(:each) do
+      allow(GoogleIDToken::Extractor).to receive(:extract) { token_data }
+      allow(User).to receive(:find_by) { dummy_user }
+    end
+    
+    it 'should return 401 if no google_id_token present in params' do
+      allow(GoogleIDToken::Extractor).to receive(:extract) { nil }
       post :create, format: :json
+      expect(response).to have_http_status(:unauthorized)
+    end
+    
+    it 'should return 401 if the token got expired' do
+      expect(User).to receive(:find_by).with(email: 'user@domain.com') { nil }
+      post :create, format: :json, google_id_token: 'dummy-token'
+      expect(response).to have_http_status(:unauthorized)
+    end
+    
+    it 'should return 401 if no user with such email' do
+      expect(User).to receive(:find_by).with(email: 'user@domain.com') { nil }
+      post :create, format: :json, google_id_token: 'dummy-token'
+      expect(response).to have_http_status(:unauthorized)
+    end
+    
+    it 'should authenticate the user based on the email from token' do
+      expect(GoogleIDToken::Extractor).to receive(:extract).with('google id token 11241') { token_data }
+      expect(User).to receive(:find_by).with(email: 'user@domain.com') { dummy_user }
+      post :create, format: :json, google_id_token: 'google id token 11241'
+      expect(response).to have_http_status(:success)
+      expect(controller.current_user).to be dummy_user
+    end
+    
+    it 'should return authenticity token' do
+      post :create, format: :json, google_id_token: 'google id token 11241'
       expect(response).to have_http_status(:success)
       body_json = JSON.parse response.body
       expect(body_json['form_authenticity_token']).to eql session[:_csrf_token]
