475c421674780108f2052d37856025ba282b4aab
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-03-17 20:01:23 +0200

    Adding dedicated class for the extractor action.

--- lib/google-id-token-extractor.rb
+++ lib/google-id-token-extractor.rb
@@ -1,9 +1,11 @@
 require 'google-id-token'
 
 module GoogleIDToken
-  Validator = GoogleIDToken::Validator.new
-  
-  def extract(token)
-    Validator.check(token, ENV['GOAUTH_CLIENT_ID'], ENV['GOAUTH_ANDROID_CLIENT_ID'])
+  class Extractor
+    Validator = GoogleIDToken::Validator.new
+    
+    def self.extract(token)
+      Validator.check(token, ENV['GOAUTH_CLIENT_ID'], ENV['GOAUTH_ANDROID_CLIENT_ID'])
+    end
   end
 end
