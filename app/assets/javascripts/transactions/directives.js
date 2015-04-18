dbfa8e9730346d98ca25339158586f8a19da288e
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-04-19 01:15:08 +0300

    Adding directive to show transfer transaction info.

--- app/assets/javascripts/transactions/directives.js
+++ app/assets/javascripts/transactions/directives.js
@@ -1,6 +1,47 @@
 !function($) {
 	var transactionsApp = angular.module('transactionsApp');
 	
+	transactionsApp.directive('showTransactionInfo', 
+	['$templateCache', '$compile', 'accounts', 'transactions', function($templateCache, $compile, accounts, transactions) {
+		var template = $templateCache.get('transfer-transaction-info.html');
+		var transferInfoLink = $compile(template);
+		var handleTransferTransaction = function(scope, element) {
+			element.mouseenter(function() {
+				element.tooltip({
+					html: true, 
+					trigger: 'manual',
+					container: 'body',
+					title: function() {
+						var linked = transferInfoLink(scope, function() {});
+						scope.$digest();
+						return linked;
+					}
+				});
+				element.on('hidden.bs.tooltip', function(e) {
+					element.tooltip('destroy');
+				});
+				element.tooltip('show');
+			});
+			element.mouseleave(function() {
+				element.tooltip('destroy');
+			})
+			scope.$on('$destroy', function() {
+				element.tooltip('destroy');
+			});
+		}
+		
+		return {
+			restrict: 'A',
+			template: '',
+			link: function(scope, element, attrs) {
+				if(scope.transaction.is_transfer) {
+					handleTransferTransaction(scope, element);
+				}
+			}
+		}
+	}]);
+
+	
 	transactionsApp.directive('moveTransactionAction', 
 	['$templateCache', '$compile', 'accounts', 'transactions', function($templateCache, $compile, accounts, transactions) {
 		var template = $templateCache.get('move-transaction.html');
