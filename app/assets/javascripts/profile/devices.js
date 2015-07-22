b20cbcfe7e873c1d931bedde4fe5736b75a95c77
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-07-22 16:32:37 +0300

    Implementing UI to reset device secret key.

--- app/assets/javascripts/profile/devices.js
+++ app/assets/javascripts/profile/devices.js
@@ -4,6 +4,17 @@
 			restrict: 'E',
 			templateUrl: 'profile/devices_table.html',
 			link: function(scope, element, attrs) {
+				scope.resetSecretKey = function(device) {
+					device.is_confirming_reset = false;
+					device.has_been_reset = false;
+					device.is_resetting = true;
+					$http.put('api/devices/' + device.id + '/reset-secret-key').success(function() {
+						device.has_been_reset = true;
+					}).finally(function() {
+						device.is_resetting = false;
+					});
+				};
+
 				$http.get('api/devices.json').success(function(data) {
 					scope.devices = data;
 				});
--- spec/javascripts/profile/devices_spec.js
+++ spec/javascripts/profile/devices_spec.js
@@ -25,9 +25,33 @@ describe('profile.devices', function() {
 			$httpBackend.expectGET('api/devices.json').respond([dev1, dev2, dev3]);
 			compile();
 			$httpBackend.flush();
-			console.log('flushed');
-			console.log(scope);
 			expect(scope.devices).toEqual([dev1, dev2, dev3]);
+		});
+
+		describe('resetSecretKey', function() {
+			beforeEach(function() {
+				dev2.is_confirming_reset = true;
+				dev2.has_been_reset = true;
+				dev2.is_resetting = false;
+				$httpBackend.whenGET('api/devices.json').respond([dev1, dev2, dev3]);
+				$httpBackend.whenPUT('api/devices/2/reset-secret-key').respond();
+				compile();
+			});
+
+			it('should update progress flags', function() {
+				scope.resetSecretKey(dev2);
+				expect(dev2.is_confirming_reset).toEqual(false);
+				expect(dev2.has_been_reset).toEqual(false);
+				expect(dev2.is_resetting).toEqual(true);
+			});
+
+			it('should send PUT request to reset the key and update flags on complete', function() {
+				$httpBackend.expectPUT('api/devices/2/reset-secret-key').respond();
+				scope.resetSecretKey(dev2);
+				$httpBackend.flush();
+				expect(dev2.has_been_reset).toEqual(true);
+				expect(dev2.is_resetting).toEqual(false);
+			});
 		});
 	});
 });
