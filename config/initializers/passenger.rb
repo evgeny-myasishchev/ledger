9c75f1a35a0e517835f20a775015800dd455eb4b
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-08-09 11:48:33 +0300

    Disconnecting on fork to errors.

--- config/initializers/passenger.rb
+++ config/initializers/passenger.rb
@@ -4,8 +4,13 @@ if defined?(PhusionPassenger)
   app = Rails.application
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
+      Rails.logger.info 'Passenger has forked worker process.'
+      ::Sequel::DATABASES.each{|db| 
+        Rails.logger.info "Disconnecting db connection"
+        db.disconnect
+      }
+      Rails.logger.info 'Restarting dispatcher...'
       app.domain_context.event_store.dispatcher.restart
-      Rails.logger.info 'Passenger has forked worker process. Events dispatcher restarted.'
     else
       # We're in direct spawning mode. We don't need to do anything.
       fail 'Direct spawning needs to be tested'
