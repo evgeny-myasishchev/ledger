7887e4d9ab906916618adb1eba02c8c859171969
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-11-18 15:54:19 +0200

    Implementing promise status filter to simplify template logic to show progress.

---
+++ app/assets/javascripts/app/shared/promiseStatusFilter.js
@@ -0,0 +1,25 @@
+(function () {  
+  'use strict';
+
+  angular
+    .module('homeApp')
+    .filter('promiseStatus', promiseStatus);
+
+  function promiseStatus() {
+    return function(promise, status) {
+      status.inProgress = true;
+      status.resolved = false;
+      status.rejected = false;
+      promise
+        .then(function() {
+          status.resolved = true;
+        })
+        .catch(function() {
+          status.rejected = true;
+        })
+        .finally(function() {
+          status.inProgress = false;
+        });
+    }
+  }
+})();
