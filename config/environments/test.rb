4cace9f5fa4d273a0ca5515f782acabe35c2c7b7
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-11-25 17:54:26 +0200

    Using custom log config in tests.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Using test specific log config
+  config.log_config_path = File.join(config.root, 'config', 'log-test.xml')
 end
