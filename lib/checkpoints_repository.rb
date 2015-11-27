ac82167bae7578a32374b5746798b2f2201130bf
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-11-27 15:36:21 +0200

    Using underscore since deps loader still doesn't handle dashes. Implementing simple in-memory repo.

---
+++ lib/checkpoints_repository.rb
@@ -0,0 +1,27 @@
+class CheckpointsRepository
+  # Retrieves checkpoint for given identifier. Returns nil if no checkpoint.
+  def get_checkpoint(identifier)
+    raise 'Not implemented'
+  end
+
+  # Saves the checkpoint for given identifier.
+  def save_checkpoint(identifier, checkpoint)
+    raise 'Not implemented'
+  end
+
+  # In Memory checkpoints repository. 
+  # To be used for testing purposes.
+  class InMemory < CheckpointsRepository
+    def initialize
+      @store = {}
+    end
+
+    def get_checkpoint(identifier) 
+      @store[identifier]
+    end
+
+    def save_checkpoint(identifier, checkpoint)
+      @store[identifier] = checkpoint
+    end
+  end
+end
