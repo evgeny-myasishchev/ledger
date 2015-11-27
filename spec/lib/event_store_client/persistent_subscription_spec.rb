515084d02a159a0d84d413c23e0844dbfc62f53a
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-11-27 15:37:37 +0200

    Using underscore since deps loader still doesn't handle dashes.

---
+++ spec/lib/event_store_client/persistent_subscription_spec.rb
@@ -0,0 +1,8 @@
+require 'rails-helper'
+
+describe EventStoreClient::PersistentSubscription do
+  describe 'pull' do
+    it 'should deliver all events starting from given checkpoint to handlers' do      
+    end
+  end
+end
