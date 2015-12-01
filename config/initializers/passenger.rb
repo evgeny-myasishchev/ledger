0624451935a3044df5e204a8ac92d776033ec77d
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-12-01 21:14:39 +0200

    Fixing passenger initializer. There is no dispatcher anymore.

--- config/initializers/passenger.rb
+++ config/initializers/passenger.rb
@@ -1,16 +1,13 @@
-# If we are hosted by passenger we need to restart the dispatcher on each fork.
-# Async dispatcher uses separate thread and forked threads are stopped.
+# If we are hosted by passenger we need to disconnect all sequel connections since they're getting broken
 if defined?(PhusionPassenger)
-  app = Rails.application
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
-      Rails.logger.info 'Passenger has forked worker process.'
+      logger = Rails.logger
+      logger.info 'Passenger has forked worker process.'
       ::Sequel::DATABASES.each{|db| 
-        Rails.logger.info "Disconnecting db connection"
+        logger.info 'Disconnecting db connection'
         db.disconnect
       }
-      Rails.logger.info 'Restarting dispatcher...'
-      app.domain_context.event_store.dispatcher.restart
     else
       # We're in direct spawning mode. We don't need to do anything.
       fail 'Direct spawning needs to be tested'
