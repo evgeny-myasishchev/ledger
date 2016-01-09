76544e08266b03b4fb95192271f799dee15244d8
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-01-09 15:08:25 +0200

    Regenerating schema so it includes postgres specific stuff.

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20151203223902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "checkpoints", force: :cascade do |t|
     t.string  "identifier",                  null: false
     t.integer "checkpoint_number", limit: 8, null: false
   end
 
-  add_index "checkpoints", ["identifier"], name: "index_checkpoints_on_identifier"
+  add_index "checkpoints", ["identifier"], name: "index_checkpoints_on_identifier", using: :btree
 
   create_table "currency_rates", force: :cascade do |t|
     t.string   "from",       null: false
@@ -28,7 +31,7 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.datetime "updated_at"
   end
 
-  add_index "currency_rates", ["from", "to"], name: "index_currency_rates_on_from_and_to", unique: true
+  add_index "currency_rates", ["from", "to"], name: "index_currency_rates_on_from_and_to", unique: true, using: :btree
 
   create_table "device_secrets", force: :cascade do |t|
     t.integer  "user_id",    null: false
@@ -39,8 +42,22 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "device_secrets", ["device_id"], name: "index_device_secrets_on_device_id", unique: true
-  add_index "device_secrets", ["user_id"], name: "index_device_secrets_on_user_id"
+  add_index "device_secrets", ["device_id"], name: "index_device_secrets_on_device_id", unique: true, using: :btree
+  add_index "device_secrets", ["user_id"], name: "index_device_secrets_on_user_id", using: :btree
+
+  create_table "event-store-commits", primary_key: "checkpoint_number", force: :cascade do |t|
+    t.string   "stream_id",        limit: 50, null: false
+    t.string   "commit_id",        limit: 50, null: false
+    t.integer  "commit_sequence",             null: false
+    t.integer  "stream_revision",             null: false
+    t.datetime "commit_timestamp",            null: false
+    t.binary   "events",                      null: false
+    t.binary   "headers",                     null: false
+  end
+
+  add_index "event-store-commits", ["stream_id", "commit_id"], name: "event-store-commits_stream_id_commit_id_index", unique: true, using: :btree
+  add_index "event-store-commits", ["stream_id", "commit_sequence"], name: "event-store-commits_stream_id_commit_sequence_index", unique: true, using: :btree
+  add_index "event-store-commits", ["stream_id", "stream_revision"], name: "event-store-commits_stream_id_stream_revision_index", unique: true, using: :btree
 
   create_table "projections_accounts", force: :cascade do |t|
     t.string  "ledger_id",                       null: false
@@ -57,9 +74,9 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.integer "pending_balance",     default: 0, null: false
   end
 
-  add_index "projections_accounts", ["aggregate_id"], name: "index_projections_accounts_on_aggregate_id", unique: true
-  add_index "projections_accounts", ["ledger_id", "sequential_number"], name: "index_projections_accounts_on_ledger_id_and_sequential_number", unique: true
-  add_index "projections_accounts", ["ledger_id"], name: "index_projections_accounts_on_ledger_id"
+  add_index "projections_accounts", ["aggregate_id"], name: "index_projections_accounts_on_aggregate_id", unique: true, using: :btree
+  add_index "projections_accounts", ["ledger_id", "sequential_number"], name: "index_projections_accounts_on_ledger_id_and_sequential_number", unique: true, using: :btree
+  add_index "projections_accounts", ["ledger_id"], name: "index_projections_accounts_on_ledger_id", using: :btree
 
   create_table "projections_categories", force: :cascade do |t|
     t.string  "ledger_id",           null: false
@@ -69,7 +86,7 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.string  "authorized_user_ids", null: false
   end
 
-  add_index "projections_categories", ["ledger_id", "category_id"], name: "index_projections_categories_on_ledger_id_and_category_id", unique: true
+  add_index "projections_categories", ["ledger_id", "category_id"], name: "index_projections_categories_on_ledger_id_and_category_id", unique: true, using: :btree
 
   create_table "projections_ledgers", force: :cascade do |t|
     t.string  "aggregate_id",        null: false
@@ -79,12 +96,7 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.string  "authorized_user_ids"
   end
 
-  add_index "projections_ledgers", ["aggregate_id"], name: "index_projections_ledgers_on_aggregate_id", unique: true
-
-  create_table "projections_meta", force: :cascade do |t|
-    t.string  "projection_id", null: false
-    t.integer "version",       null: false
-  end
+  add_index "projections_ledgers", ["aggregate_id"], name: "index_projections_ledgers_on_aggregate_id", unique: true, using: :btree
 
   create_table "projections_pending_transactions", force: :cascade do |t|
     t.string   "transaction_id", null: false
@@ -97,8 +109,8 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.integer  "type_id",        null: false
   end
 
-  add_index "projections_pending_transactions", ["transaction_id"], name: "index_projections_pending_transactions_on_transaction_id", unique: true
-  add_index "projections_pending_transactions", ["user_id"], name: "index_projections_pending_transactions_on_user_id"
+  add_index "projections_pending_transactions", ["transaction_id"], name: "index_projections_pending_transactions_on_transaction_id", unique: true, using: :btree
+  add_index "projections_pending_transactions", ["user_id"], name: "index_projections_pending_transactions_on_user_id", using: :btree
 
   create_table "projections_tags", force: :cascade do |t|
     t.string  "ledger_id",           null: false
@@ -107,7 +119,7 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.string  "authorized_user_ids", null: false
   end
 
-  add_index "projections_tags", ["ledger_id", "tag_id"], name: "index_projections_tags_on_ledger_id_and_tag_id", unique: true
+  add_index "projections_tags", ["ledger_id", "tag_id"], name: "index_projections_tags_on_ledger_id_and_tag_id", unique: true, using: :btree
 
   create_table "projections_transactions", force: :cascade do |t|
     t.string   "transaction_id",                           null: false
@@ -128,9 +140,9 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.boolean  "is_pending",               default: false, null: false
   end
 
-  add_index "projections_transactions", ["account_id"], name: "index_projections_transactions_on_account_id"
-  add_index "projections_transactions", ["reported_by_id"], name: "index_projections_transactions_on_reported_by_id"
-  add_index "projections_transactions", ["transaction_id"], name: "index_projections_transactions_on_transaction_id", unique: true
+  add_index "projections_transactions", ["account_id"], name: "index_projections_transactions_on_account_id", using: :btree
+  add_index "projections_transactions", ["reported_by_id"], name: "index_projections_transactions_on_reported_by_id", using: :btree
+  add_index "projections_transactions", ["transaction_id"], name: "index_projections_transactions_on_transaction_id", unique: true, using: :btree
 
   create_table "snapshots", force: :cascade do |t|
     t.string  "aggregate_id", null: false
@@ -138,7 +150,7 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.binary  "data",         null: false
   end
 
-  add_index "snapshots", ["aggregate_id"], name: "index_snapshots_on_aggregate_id", unique: true
+  add_index "snapshots", ["aggregate_id"], name: "index_snapshots_on_aggregate_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -155,7 +167,8 @@ ActiveRecord::Schema.define(version: 20151203223902) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "projections_transactions", "users", column: "reported_by_id"
 end
