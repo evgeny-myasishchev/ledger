971e70a85d9161f3066c02155a59a06e3f80edff
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-05-29 00:49:00 +0300

    Removing unneeded stuff.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -12,25 +12,7 @@ elif [ "$1" = 'backburner' ]; then
   gosu ledger backburner
 elif [ "$1" = 'db-setup' ]; then
   echo 'doing db setup...'
-  : "${PGHOST:?PGHOST needs to be provided.}"
-  : "${PGPASSWORD:?PGPASSWORD needs to be provided.}"
-  : "${LEDGER_PGPASS:?LEDGER_PGPASS needs to be provided.}"
-  CREATE_ROLE_SQL="DO
-  \$body\$
-  BEGIN
-     IF NOT EXISTS (
-        SELECT *
-        FROM   pg_catalog.pg_user
-        WHERE  usename = 'ledger') THEN
-
-        CREATE ROLE ledger LOGIN PASSWORD '${LEDGER_PGPASS}' CREATEDB;
-     END IF;
-  END
-  \$body\$;
-  "
-  psql -U postgres -c "${CREATE_ROLE_SQL}"
   cd /apps/ledger/app
-  gosu ledger bundle exec rake db:create
   gosu ledger bundle exec rake db:setup
 else
   exec "$@"
