c243fa24e56c52fad625a8e7cb034683b5a41ea5
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-04 00:07:47 +0300

    Adding capistrano tasks to perform full deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,8 +8,9 @@ server 'my-ledger.com', roles: %w(app), user: fetch(:local_user), port: 2200
 # set config below on a per stage basis
 # set :config_root, './ledger/env'
 # set :web_container, 'ledger-env-web'
-# set :web_container_port, 6000
+# set :web_container_port, 5000
 # set :worker_container, 'ledger-env-worker'
+# set :docker_network, 'ledger-env'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -19,6 +20,16 @@ server 'my-ledger.com', roles: %w(app), user: fetch(:local_user), port: 2200
 
 # Default value for :pty is false
 # set :pty, true
+
+desc 'Deploy new version of ledger containers'
+task :deploy do
+  on roles(:app) do
+    execute 'docker', 'pull', fetch(:docker_repo)
+    invoke 'deploy:stop'
+    invoke 'deploy:remove_containers'
+    invoke 'deploy:create_containers'
+  end
+end
 
 namespace :deploy do
   desc 'Restart containers'
@@ -35,6 +46,18 @@ namespace :deploy do
     invoke 'deploy:web:stop'
   end
 
+  desc 'Create web and worker Containers'
+  task :create_containers do
+    invoke 'deploy:worker:create_container'
+    invoke 'deploy:web:create_container'
+  end
+
+  desc 'Remove web and worker Containers'
+  task :remove_containers do
+    invoke 'deploy:worker:remove_container'
+    invoke 'deploy:web:remove_container'
+  end
+
   namespace :worker do
     desc 'Stop worker container'
     task :stop do
@@ -47,6 +70,25 @@ namespace :deploy do
     task :start do
       on roles(:app) do
         execute 'docker', 'start', fetch(:worker_container)
+      end
+    end
+
+    desc 'Remove worker container'
+    task :remove_container do
+      on roles(:app) do
+        execute 'docker', 'rm', fetch(:worker_container)
+      end
+    end
+
+    desc 'Create worker container'
+    task :create_container do
+      on roles(:app) do
+        execute [
+          'docker run -d --restart=unless-stopped',
+          '--env-file', File.join(fetch(:config_root), 'app.env'),
+          '--net', fetch(:docker_network),
+          '--name', fetch(:worker_container), fetch(:docker_repo)
+        ].join(' ')
       end
     end
   end
@@ -65,18 +107,25 @@ namespace :deploy do
         execute 'docker', 'start', fetch(:web_container)
       end
     end
+
+    desc 'Remove worker container'
+    task :remove_container do
+      on roles(:app) do
+        execute 'docker', 'rm', fetch(:web_container)
+      end
+    end
+
+    desc 'Create worker container'
+    task :create_container do
+      on roles(:app) do
+        execute [
+          'docker run -d --restart=unless-stopped',
+          '--env-file', File.join(fetch(:config_root), 'app.env'),
+          '-p ', "#{fetch(:web_container_port)}:3000",
+          '--net', fetch(:docker_network),
+          '--name', fetch(:web_container), fetch(:docker_repo)
+        ].join(' ')
+      end
+    end
   end
 end
-
-# def createContainer
-#   envFile = shipit.config.workspace + '/app.env'
-#   port = shipit.config.port
-#   repo = shipit.config.dockerRepository
-#   container = shipit.config.container
-#   shipit.remote([
-#     'docker run -d --restart=unless-stopped',
-#     '--env-file', envFile,
-#     '-p ' + port + ':3000',
-#     '--name ', container, repo
-#   ].join(' '))
-# end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,6 @@ roles :app
 
 set :config_root, './ledger/stage'
 set :web_container, 'ledger-staging-web'
-set :web_container_port, 6000
+set :web_container_port, 5010
 set :worker_container, 'ledger-staging-worker'
+set :docker_network, 'ledger-staging'
