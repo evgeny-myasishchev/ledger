30930bb417553b579bad2ce163038f3a014b13f2
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-09 16:47:22 +0300

    Implementing logic to extract access token from raw jwt token.

--- lib/access_token.rb
+++ lib/access_token.rb
@@ -1,5 +1,7 @@
 class AccessToken
+  attr_reader :payload
   def initialize(payload)
+    @payload = payload
   end
 
   def ensure_audience!(client_id)
@@ -7,6 +9,15 @@ class AccessToken
 
   class << self
     def extract(raw_jwt_token, certificates)
+      decoded_token = nil
+      certificates.each do |cert|
+        begin
+          decoded_token = JWT.decode raw_jwt_token, cert.public_key
+        rescue JWT::VerificationError
+          raise if certificates.last == cert
+        end
+      end
+      new decoded_token[0]
     end
   end
 end
--- spec/lib/access_token_spec.rb
+++ spec/lib/access_token_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe AccessToken do
+  let(:rsa_private) { OpenSSL::PKey::RSA.generate 2048 }
+  let(:rsa_public) { rsa_private.public_key }
+
+  let(:payload) do
+    {
+      'aud' => FFaker::Internet.domain_name
+    }
+  end
+
   describe 'ensure_audience!' do
     it 'should raise error if audience mismatch' do
     end
@@ -10,10 +19,34 @@ describe AccessToken do
   end
 
   describe 'extract' do
+    let(:raw_jwt_token) { JWT.encode payload, rsa_private, 'RS256' }
+
+    let(:valid_cert) do
+      cert = OpenSSL::X509::Certificate.new
+      cert.public_key = rsa_public
+      cert
+    end
+
+    let(:invalid_cert) do
+      cert = OpenSSL::X509::Certificate.new
+      cert.public_key = OpenSSL::PKey::RSA.generate(2048).public_key
+      cert
+    end
+
     it 'should decode provided raw JWT data and create new instance' do
+      access_token = described_class.extract raw_jwt_token, [valid_cert]
+      expect(access_token.payload).to eql(payload)
+    end
+
+    it 'should support multiple certificates when decoding' do
+      access_token = described_class.extract raw_jwt_token, [invalid_cert, valid_cert]
+      expect(access_token.payload).to eql(payload)
     end
 
     it 'should raise error if certificates does not match' do
+      expect(lambda do
+        described_class.extract raw_jwt_token, [invalid_cert]
+      end).to raise_error(JWT::VerificationError)
     end
   end
 end
