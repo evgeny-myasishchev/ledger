d7b15c8b76a3409185c0c84b546503cd1aed2776
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-07-15 08:53:15 +0300

    Fixing linting errors.

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -1,40 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe Api::SessionsController, :type => :controller do
-
-  describe "routes", :type => :routing do
-    it "routes POST create session" do
-      expect({post: 'api/sessions'}).to route_to controller: 'api/sessions', action: 'create'
+RSpec.describe Api::SessionsController, type: :controller do
+  describe 'routes', type: :routing do
+    it 'routes POST create session' do
+      expect(post: 'api/sessions').to route_to controller: 'api/sessions', action: 'create'
     end
   end
-  
-  describe "POST create" do
-    let(:token_data) { 
-      {'email' => 'user@domain.com'}
-    }
-    let(:dummy_user) { User.new id: 12432121, email: token_data['email'] }
+
+  describe 'POST create' do
+    let(:token_data) do
+      { 'email' => 'user@domain.com' }
+    end
+    let(:dummy_user) { User.new id: 12_432_121, email: token_data['email'] }
     before(:each) do
       allow(GoogleIDToken::Extractor).to receive(:extract) { token_data }
       allow(User).to receive(:find_by) { dummy_user }
     end
-    
+
     it 'should return 401 if no google_id_token present in params' do
       post :create, format: :json
       expect(response).to have_http_status(:unauthorized)
     end
-    
+
     it 'should return 401 if the token is invalid' do
       allow(GoogleIDToken::Extractor).to receive(:extract).with('dummy-token').and_raise GoogleIDToken::InvalidTokenException.new 'invalid token'
       post :create, format: :json, google_id_token: 'dummy-token'
       expect(response).to have_http_status(:unauthorized)
     end
-    
+
     it 'should return 401 if no user with such email' do
       expect(User).to receive(:find_by).with(email: 'user@domain.com') { nil }
       post :create, format: :json, google_id_token: 'dummy-token'
       expect(response).to have_http_status(:unauthorized)
     end
-    
+
     it 'should authenticate the user based on the email from token' do
       expect(GoogleIDToken::Extractor).to receive(:extract).with('google id token 11241') { token_data }
       expect(User).to receive(:find_by).with(email: 'user@domain.com') { dummy_user }
@@ -42,9 +41,9 @@ RSpec.describe Api::SessionsController, :type => :controller do
       expect(response).to have_http_status(:success)
       expect(controller.current_user).to be dummy_user
     end
-    
+
     it 'should return authenticity token' do
-      expect(controller).to receive(:form_authenticity_token) { 'form-authenticity-token-10032'}
+      expect(controller).to receive(:form_authenticity_token) { 'form-authenticity-token-10032' }
       post :create, format: :json, google_id_token: 'google id token 11241'
       expect(response).to have_http_status(:success)
       body_json = JSON.parse response.body
