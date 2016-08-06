e67e084f160890f44e616a0480ecafe4c08f126a
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-08-07 01:04:07 +0300

    Implementing google certificates provider.

---
+++ lib/access_token/certificates.rb
@@ -0,0 +1,71 @@
+class AccessToken::Certificates
+  class << self
+    def providers
+      {
+        'https://accounts.google.com' => GoogleProvider.new
+      }.freeze
+    end
+
+    def self.get_certificate(_jwt_header, _jwt_body)
+    end
+  end
+
+  class GoogleProvider
+    include Loggable
+    GOOGLE_CERTS_URI = 'https://www.googleapis.com/oauth2/v1/certs'.freeze
+
+    attr_reader :cache
+
+    def initialize(initial_certificates = {}, verbose: false)
+      @cache = create_cache
+      @cache.merge! initial_certificates
+      @verbose = verbose
+    end
+
+    def get_certificate(jwt_header)
+      kid = jwt_header['kid']
+      unless cache.key?(kid)
+        # TODO: Multithreading may be an issue here. Investigation is required
+        logger.debug "Certificate for kid: #{kid} not found. Refreshing from google api..."
+        certs_before = cache.length
+        cache.merge! fetch_certificates
+        fetched_count = cache.length - certs_before
+        logger.debug "Fetched #{fetched_count} new certificates"
+        remove_expired_certificates_from_cache cache
+      end
+
+      cache[kid]
+    end
+
+    private
+
+    def create_cache
+      Hash.new do |_hash, key|
+        logger.info "Certificate with kid=#{key} not found"
+        raise AccessToken::TokenError, "Certificate with kid=#{key} not found"
+      end
+    end
+
+    def fetch_certificates
+      uri = URI(GOOGLE_CERTS_URI)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      res = http.request(Net::HTTP::Get.new(uri.request_uri))
+      raise "Failed to get certificates: #{res.code} - #{res.message}" unless res.is_a?(Net::HTTPSuccess)
+      logger.debug("Got response: #{res.body}") if @verbose
+      Hash[MultiJson.load(res.body).map do |key, cert|
+             [key, OpenSSL::X509::Certificate.new(cert)]
+           end]
+    end
+
+    def remove_expired_certificates_from_cache(cache)
+      cache.delete_if do |kid|
+        cert = cache[kid]
+        if cert.not_after <= Time.now
+          logger.debug "Removing expired certificate from cache. Kid=#{kid}, subject: #{cert.subject}, not_after: #{cert.not_after}"
+          true
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/access_token/certificates_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe AccessToken::Certificates do
+  describe AccessToken::Certificates::GoogleProvider do
+    describe 'get_certificate' do
+      let(:kid1) { random_string('kid1') }
+      let(:kid2) { random_string('kid2') }
+      let(:kid3) { random_string('kid3') }
+      let(:initial_certs) do
+        {
+          kid1 => create_x509_cert,
+          kid2 => create_x509_cert,
+          kid3 => create_x509_cert
+        }
+      end
+
+      let(:new_kid) { random_string('kid') }
+      subject { described_class.new(initial_certs, verbose: true) }
+
+      before(:each) do
+        @api_call = stub_request(:get, described_class::GOOGLE_CERTS_URI)
+                    .to_return(body: {
+                      new_kid => create_x509_cert.to_s
+                    }.to_json)
+      end
+
+      it 'should return matching certificate for given kid' do
+        expect(subject.get_certificate('kid' => kid2)).to eql initial_certs[kid2]
+        expect(subject.get_certificate('kid' => kid3)).to eql initial_certs[kid3]
+      end
+
+      it 'should fetch and cache new certificates from google api if no matching certificate found' do
+        new_kid1 = random_string('new-kid1')
+        new_cert_1 = create_x509_cert
+        new_kid2 = random_string('new-kid2')
+        new_cert_2 = create_x509_cert
+        @api_call = stub_request(:get, described_class::GOOGLE_CERTS_URI)
+                    .to_return(body: {
+                      new_kid1 => new_cert_1.to_s,
+                      new_kid2 => new_cert_2.to_s
+                    }.to_json)
+        expect(subject.get_certificate('kid' => new_kid1).subject).to eql new_cert_1.subject
+        expect(subject.get_certificate('kid' => new_kid2).subject).to eql new_cert_2.subject
+        expect(@api_call).to have_been_made.once
+        expect(subject.cache[new_kid1].subject).to eql new_cert_1.subject
+        expect(subject.cache[new_kid2].subject).to eql new_cert_2.subject
+      end
+
+      it 'should remove expired certificates from cache' do
+        subject.cache[kid1] = create_x509_cert not_after: Time.now - 1
+        subject.get_certificate 'kid' => new_kid
+        expect(subject.cache).not_to have_key kid1
+      end
+
+      it 'should raise error google response was not 200' do
+        stub_request(:get, described_class::GOOGLE_CERTS_URI).to_return(status: [500, 'Internal Server Error'])
+        expect { subject.get_certificate('kid' => random_string) }
+          .to raise_error RuntimeError, 'Failed to get certificates: 500 - Internal Server Error'
+      end
+
+      it 'should raise TokenError if required cert not found' do
+        not_existing_kid = random_string('not-existing-kid')
+        expect { subject.get_certificate('kid' => not_existing_kid) }
+          .to raise_error AccessToken::TokenError, "Certificate with kid=#{not_existing_kid} not found"
+      end
+    end
+  end
+end
