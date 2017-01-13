b35c22ede247e77ed3aef042b833c95f452a45e7
Eugene Myasyshchev <Eugene.Myasyshchev@dataart.com>
2017-01-13 09:42:03 +0200

    Adding some comments.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,6 +8,13 @@ if [ "$1" = 'passenger-start' ]; then
 
   echo 'Starting passenger'
   passenger start --user ledger -p 3000
+  
+  # Riping out prefix added by passenger can be done like below
+  # $ passenger start | sed -e 's/App [0-9]\+ \(stdout\|stderr\): //'
+  # However this doesn't work well with non daemon mode and Ctrl + C
+  #
+  # I've also raised this: https://github.com/phusion/passenger/issues/1915
+  
 elif [ "$1" = 'backburner' ]; then
   gosu ledger backburner
 elif [ "$1" = 'db-setup' ]; then
