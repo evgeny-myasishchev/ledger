2dc79f92fc48dffb4bb719c8c536cf51dbd332bc
Eugene Myasyshchev <Eugene.Myasyshchev@dataart.com>
2017-01-13 10:59:05 +0200

    Adding helper script.

---
+++ test/bin/start-stage-container.sh
@@ -0,0 +1,5 @@
+docker run -it --rm \
+    --log-driver syslog --log-opt syslog-address=udp://localhost:15140 --log-opt syslog-format=rfc3164 \
+    --log-opt tag="docker.ruby.json-ledger.{{.Name}}" \
+    --env-file app.env --net ledger-stage \
+    --name ledger-stage-web -p 3000:3000 evgenymyasishchev/ledger
