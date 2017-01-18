f40ea8982d40766b5af97410eadbe0df65e411c7
Eugene Myasyshchev <Eugene.Myasyshchev@dataart.com>
2017-01-18 11:13:26 +0200

    Using standardized tags.

--- test/bin/start-stage-container.sh
+++ test/bin/start-stage-container.sh
@@ -1,5 +1,5 @@
 docker run -it --rm \
     --log-driver syslog --log-opt syslog-address=udp://localhost:15140 --log-opt syslog-format=rfc3164 \
-    --log-opt tag="docker.passenger.ruby.json.{{.Name}}" \
+    --log-opt tag="passenger.ruby-json.staging.ledger.web" \
     --env-file app.env --net ledger-stage \
     --name ledger-stage-web -p 3000:3000 evgenymyasishchev/ledger
